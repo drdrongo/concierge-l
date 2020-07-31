404dc6033db4449698f565919fbb2373be386518
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-31 17:14:10 +0900

    article model and migration done properly this time

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_080956) do
+ActiveRecord::Schema.define(version: 2020_07_31_081345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "hotels", force: :cascade do |t|
     t.string "address"
