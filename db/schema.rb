b3a91275dd1558eba071833496133f4073949d12
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-31 17:08:49 +0900

    hotel model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_103436) do
+ActiveRecord::Schema.define(version: 2020_07_31_080712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "address"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
