b9a481c4f074faaeb3ae2994ef8068396f209fe0
Hayato Clarke <hayatoclarke@gmail.com>
2020-08-05 19:57:11 +0900

    API accessible

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,18 @@
+require 'json'
+require 'rest-client'
+
 class ReservationsController < ApplicationController
   def index
-    # @reservations = Reservation.where(user: current_user)
     @upcoming_reservations = Reservation.where(user: current_user, past: false)
     @past_reservations = Reservation.where(user: current_user, past: true)
   end
 
   def show
+    @bookings = request_api("https://www.beds24.com/api/json/getBookings")
+    # @booking = json.key("908914960")
+    @booking = @bookings.find { |booking| booking["apiReference"] == '915566853' }
+    # @booking = @bookings[0..2]
+
     @reservation = Reservation.find(params[:id])
     @articles = @reservation.hotel.articles
   end
@@ -14,4 +21,16 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new
   end
 
+  def request_api(url)
+    payload = {
+      "authentication": {
+        "apiKey": "bropdQL9vXILFXr6Iwr1urG8m",
+        "propKey": "xmyTyn0oIvRZG09yvwcQk1Zlj"
+      },
+      "includeInvoice": false,
+      "includeInfoItems": false
+    }
+    response = RestClient.post(url, payload.to_json, content_type: :json)
+    return JSON.parse(response)
+  end
 end
