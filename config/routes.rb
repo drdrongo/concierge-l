89e8f1743fb05bfc1583a4488423bdbd93fa0d4a
Hayato Clarke <hayatoclarke@gmail.com>
2020-08-06 13:23:16 +0900

    more merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-<<<<<<< HEAD
-  resources :reservations, only: %i[ index show update ]
-=======
-  resources :reservations, only: %i[ index show new create ]
->>>>>>> master
+  resources :reservations, only: %i[ index show new create update ]
   resources :articles, only: %i[ show ]
 end
