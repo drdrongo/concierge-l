096bcaec43c29f0edd04e544f6e1ea398e5c88fa
Shinya Tawata <tawatashinya@gmail.com>
2020-08-13 22:03:55 +0900

    amenities button fixed

--- app/controllers/hotel_amenities_controller.rb
+++ app/controllers/hotel_amenities_controller.rb
@@ -2,6 +2,7 @@ class HotelAmenitiesController < ApplicationController
   def index
     @reservation = Reservation.find(params[:reservation_id])
     @hotel_amenities = @reservation.hotel.hotel_amenities
+    @hotel_amenities = @hotel_amenities.includes(:amenity).where(amenities: { category: params[:category] })
     @request = Request.new
   end
 end
