96427ef0f81daafc2b93b6aa50bf8b63b4624f40
Hayato Clarke <hayatoclarke@gmail.com>
2020-08-20 02:23:35 +0900

    swiper slides working and functional.

--- app/controllers/hotel_amenities_controller.rb
+++ app/controllers/hotel_amenities_controller.rb
@@ -3,8 +3,17 @@ class HotelAmenitiesController < ApplicationController
     @reservation = Reservation.find(params[:reservation_id])
     @back_path = {controller: "reservations", action: "show", id: @reservation.id}
 
-    @hotel_amenities = @reservation.hotel.hotel_amenities
-    @hotel_amenities = @hotel_amenities.includes(:amenity).where(amenities: { category: params[:category] })
+    @hotel_amenities = @reservation.hotel.hotel_amenities.includes(:amenity)
+    
+    @requestables = @hotel_amenities.where(hotel_amenities: { requestable: true })
+    @essentials = @hotel_amenities.where(amenities: { category: 'Essentials' }, hotel_amenities: { requestable: false })
+    @bed_bath = @hotel_amenities.where(amenities: { category: 'Bed & Bath' }, hotel_amenities: { requestable: false })
+    @kitchen = @hotel_amenities.where(amenities: { category: 'Kitchen' }, hotel_amenities: { requestable: false })
+    @other = @hotel_amenities.where(amenities: { category: 'Other' }, hotel_amenities: { requestable: false })
+
+
+    @hotel_amenities = @hotel_amenities.includes(:amenity).where(amenities: { category: params[:category]}, hotel_amenities: { requestable: false })
+    
     @request = Request.new
   end
 end
