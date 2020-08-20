4b874b6eb1d1eed0966d28245004df659108a76d
Hayato Clarke <hayatoclarke@gmail.com>
2020-08-20 14:32:04 +0900

    frontend cleanup and functionality.

--- app/controllers/hotel_amenities_controller.rb
+++ app/controllers/hotel_amenities_controller.rb
@@ -1,19 +1,18 @@
 class HotelAmenitiesController < ApplicationController
   def index
     @reservation = Reservation.find(params[:reservation_id])
-    @back_path = {controller: "reservations", action: "show", id: @reservation.id}
+    @request = Request.new
 
     @hotel_amenities = @reservation.hotel.hotel_amenities.includes(:amenity)
-    
     @requestables = @hotel_amenities.where(hotel_amenities: { requestable: true })
-    @essentials = @hotel_amenities.where(amenities: { category: 'Essentials' }, hotel_amenities: { requestable: false })
-    @bed_bath = @hotel_amenities.where(amenities: { category: 'Bed & Bath' }, hotel_amenities: { requestable: false })
-    @kitchen = @hotel_amenities.where(amenities: { category: 'Kitchen' }, hotel_amenities: { requestable: false })
-    @other = @hotel_amenities.where(amenities: { category: 'Other' }, hotel_amenities: { requestable: false })
 
+    @hotel_amenities = @hotel_amenities.where(hotel_amenities: { requestable: false })
 
-    @hotel_amenities = @hotel_amenities.includes(:amenity).where(amenities: { category: params[:category]}, hotel_amenities: { requestable: false })
-    
-    @request = Request.new
+    @essentials = @hotel_amenities.where(amenities: { category: 'Essentials' })
+    @bed_bath = @hotel_amenities.where(amenities: { category: 'Bed & Bath' })
+    @kitchen = @hotel_amenities.where(amenities: { category: 'Kitchen' })
+    @other = @hotel_amenities.where(amenities: { category: 'Other' })
+
+    # @hotel_amenities = @hotel_amenities.includes(:amenity).where(amenities: { category: params[:category]}, hotel_amenities: { requestable: false })
   end
 end
