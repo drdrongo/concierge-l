1fd14c6a3419af6c661e0b72c1893c39690ced7e
Hayato Clarke <hayatoclarke@gmail.com>
2020-08-22 11:49:16 +0900

    added default values but they wont work properly

--- db/migrate/20200822015955_change_default_times_for_reservations.rb
+++ db/migrate/20200822015955_change_default_times_for_reservations.rb
@@ -1,6 +1,7 @@
 class ChangeDefaultTimesForReservations < ActiveRecord::Migration[6.0]
   def change
-    change_column :reservations, :arrival_time, :time, default: Time.new(2000, 1, 1, 15)
-    change_column :reservations, :departure_time, :time, default: Time.new(2000, 1, 1, 11)
+    Time.zone = "Asia/Tokyo"
+    change_column :reservations, :arrival_time, :time, default: Time.new(2000, 1, 1, 24)
+    change_column :reservations, :departure_time, :time, default: Time.new(2000, 1, 1, 20)
   end
 end
