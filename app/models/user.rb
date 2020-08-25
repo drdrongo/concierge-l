3d485773871d65d700034c9d018c08bf18116c51
Hayato Clarke <hayatoclarke@gmail.com>
2020-08-25 21:27:22 +0900

    navbar and footer and sizing changes for home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :reservations, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :tickets, dependent: :destroy
   has_many :events, through: :tickets
   has_one_attached :photo
   has_many :events_as_host, foreign_key: :user_id, class_name: "Event"
-
 end
